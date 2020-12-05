eb68108658299304c60dd774ef02f8941dd6bb09
James Walsh <jamesgcwalsh@gmail.com>
2020-12-05 03:39:36 -0500

    Update main.c
    formatted output

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int argc, char* argv[])
   bank_data *ban_dat = malloc(sizeof(bank_data));
   //puts("About to call read_input_file()");
   read_input_file(argv[1], ban_dat, threadedMode);
-  puts("Back in main from read_input_file()");
+  puts("Back in main from read_input_file()\n");
   print_formatted_output(ban_dat);
 
   if (access(FILENAME_OUTPUT, F_OK) != -1)
